039b24802d695ff52b74bd8057f6032a77b2b0b5
Muhammad-Asim-raza <asimraza9659@gmail.com>
2025-12-19 02:11:47 -0500

    Area of rectangle practice

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,21 @@
+let length;
+let width;
+function calculateArea(){
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    
+    let area = length * width; 
+    document.getElementById('result').innerText= `The area of the rectangle is: ${area}`;
+ }
+
+ let grocery1;
+ let grocery2;
+ let grocery3;
+ function TotalAmount(){
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    let Totalamount= grocery1 + grocery2 + grocery3;
+    document.getElementById('Total').innerText = `The total amount is : ${Totalamount}`;
+ }
---
+++ controlFlow/control_flow.js
@@ -0,0 +1,57 @@
+let userRole = "admin";
+let accessLevel;
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+console.log("Access Level:", accessLevel);
+let isLoggedIn = true;
+let userMessage;
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+let userType = "subscriber";
+let userCategory;
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+console.log("User Category:", userCategory);
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+console.log("Authentication Status:", authenticationStatus);
+let roll= "Employee";
+let access;
+if(roll=== "Employee"){
+    access="Have an access to Dietary Services";
+}
+else if(roll==="Enrolled Member"){
+    access="Have access to Dietary Services and one-on-one interaction with a dietician.";
+}else if(roll==="Subscriber"){
+    access=" Have partial access to facilitate Dietary Services only.";
+}
+else if(roll==="Non-Subscriber"){
+    access="need to enroll or at least subscribe first to avail this facility.";
+}else{
+    access="Not defined";
+}
+console.log("Access: ", access);
