a7ec465b6613dff0d22885ec0b849327ad72a3ce
Muhammad-Asim-raza <asimraza9659@gmail.com>
2026-01-26 07:08:07 -0800

    10.Develop Colorful Memory Match Game Using JavaScript DOM Lab

--- 10.colorfulMemoryGame/colorful_memory_game.js
+++ 10.colorfulMemoryGame/colorful_memory_game.js
@@ -8,7 +8,7 @@ const startbtn = document.getElementById('startbtn');
 const gameContainer = document.getElementById('game-container');
 const scoreElement = document.getElementById('score');
 const timerElement = document.getElementById('timer');
- function generateCards() {
+ function generateCards()  {
            for (const color of cards) {
                const card = document.createElement('div');
                card.classList.add('card');
