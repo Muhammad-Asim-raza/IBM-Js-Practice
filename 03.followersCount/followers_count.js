085c33042ea9746aa488efdd5cd85b57235e4347
Muhammad-Asim-raza <asimraza9659@gmail.com>
2026-02-01 21:46:36 -0800

    03.functions practice Lab.

--- 03.followersCount/followers_count.js
+++ 03.followersCount/followers_count.js
@@ -2,7 +2,7 @@ let count = 0; // Initialize count to 0
 function increaseCount() {
   count++; // Increment the count by 1
 displayCount(); // Display the count
-checkCountValue(); 
+checkCountValue();
 }
 function checkCountValue() {
   if (count === 10) {
